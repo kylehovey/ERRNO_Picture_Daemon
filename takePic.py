bad449293b2ffb6e8d81d9623aab5018c8be1ccf
Kyle Hovey <khoveyc@gmail.com>
2017-07-27 02:00:10 +0000

    Added initial script.

---
+++ takePic.py
@@ -0,0 +1,76 @@
+# Picture capture script for the ERRNO cubesat
+#
+# Requires a serial connection to initiate the main loop. This is done by
+#   an Arduino connected over GPIO.
+
+
+# ===== Dependencies ===== #
+import serial, picamera, json
+from RPi import GPIO
+from time import time, sleep
+from os import chdir
+# ===== Dependencies ===== #
+
+
+# ===== Globals ===== #
+SCRIPT_DIR = "/home/pi/picture_daemon"
+# ===== Globals ===== #
+
+
+# ===== Function Definition ===== #
+# Load a JSON file into a python variable
+def load_json(save_file):
+  state = open(save_file, "r")
+  contents = json.load(state)
+  state.close()
+
+  return contents
+
+# Load a JSON file into a python variable
+def save_json(save_file, contents):
+  out = open(save_file, "w")
+  json.dump(contents, out)
+  out.close()
+
+# Take a picture and store it in the current working directory
+def takePicture():
+  camera.capture(str(time()).replace(".", "") + '.jpg')
+  return
+# ===== Function Definition ===== #
+
+
+# ===== Main Section ===== #
+if __name__ == "__main__":
+  # Grab configuration
+  config = load_json("{}/config.json".format(SCRIPT_DIR))
+
+  # Change to the correct directory
+  chdir("/home/pi/picture_daemon/{}".format(config["picture_dir"]))
+
+  # Configure the serial connection
+  ser = serial.Serial(config["serial_port"], config["baud_rate"])
+  ser.baudrate = config["baud_rate"]
+
+  # Get the camera connection
+  camera = picamera.PiCamera()
+
+  # Set the GPIO mode
+  GPIO.setmode(GPIO.BOARD)
+
+  # Block the thread for a while until we get a line
+  read_ser = ser.readline()
+
+  # Determine the wait time
+  wait_ms = config["wait_time_minutes"] * 60 * 1000;
+
+  # If the command recieved is the one to initiate the pictures, start capture
+  if(read_ser == config["start_cmd"]):
+    while True:
+      # Do the thing
+      takePicture()
+
+      # Send reply back confirming picture was taken
+
+      # Sleep for five minutes
+      sleep()
+# ===== Main Section ===== #
